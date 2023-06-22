5fcdc2cdc4ce51d1de65c1984c5e40c80edf1a42
Haneenmohamed8 <125806698+Haneenmohamed8@users.noreply.github.com>
2023-06-23 02:22:19 +0300

    Update opcodes.c

--- opcodes.c
+++ opcodes.c
@@ -7,17 +7,16 @@
  */
 void pint(stack_t **stack, unsigned int line_number)
 {
-        if (*stack == NULL)
+	if (*stack == NULL)
         {
-                fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
-                fclose(bus.file);
-                free(bus.content);
-                free_stack(*stack);
-                exit(EXIT_FAILURE);
-        }
-        printf("%d\n", (*stack)->n);
+		fprintf(stderr, "L%u: can't pint, stack empty\n", line_number);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*stack);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*stack)->n);
 }
-
 
 
 
@@ -28,22 +27,22 @@ void pint(stack_t **stack, unsigned int line_number)
  */
 void pop(stack_t **stack, unsigned int line_number)
 {
-        stack_t *temp;
+	stack_t *temp;
 
-        if (*stack == NULL)
-        {
-                fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
-                fclose(bus.file);
-                free(bus.content);
-                free_stack(*stack);
-                exit(EXIT_FAILURE);
-        }
+	if (*stack == NULL)
+	{
+		fprintf(stderr, "L%u: can't pop an empty stack\n", line_number);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*stack);
+		exit(EXIT_FAILURE);
+	}
 
-        temp = *stack;
-        *stack = (*stack)->next;
-        if (*stack != NULL)
-                (*stack)->prev = NULL;
-        free(temp);
+	temp = *stack;
+	*stack = (*stack)->next;
+	if (*stack != NULL)
+		(*stack)->prev = NULL;
+	free(temp);
 }
 
 
@@ -56,25 +55,25 @@ void pop(stack_t **stack, unsigned int line_number)
  */
 void swap(stack_t **stack, unsigned int line_number)
 {
-        stack_t *temp;
+	stack_t *temp;
 
-        if (*stack == NULL || (*stack)->next == NULL)
+	if (*stack == NULL || (*stack)->next == NULL)
         {
-                fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
-                fclose(bus.file);
-                free(bus.content);
-                free_stack(*stack);
-                exit(EXIT_FAILURE);
+		fprintf(stderr, "L%u: can't swap, stack too short\n", line_number);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*stack);
+		exit(EXIT_FAILURE);
         }
 
-        temp = (*stack)->next;
-        (*stack)->next = temp->next;
-        if (temp->next != NULL)
-                temp->next->prev = *stack;
-        temp->prev = NULL;
-        temp->next = *stack;
-        (*stack)->prev = temp;
-        *stack = temp;
+	temp = (*stack)->next;
+	(*stack)->next = temp->next;
+	if (temp->next != NULL)
+		temp->next->prev = *stack;
+	temp->prev = NULL;
+	temp->next = *stack;
+	(*stack)->prev = temp;
+	*stack = temp;
 }
 
 
@@ -88,17 +87,16 @@ void swap(stack_t **stack, unsigned int line_number)
  */
 void add(stack_t **head, unsigned int counter)
 {
-        if (*head == NULL || (*head)->next == NULL)
-        {
-                fprintf(stderr, "L%u: can't add, stack too short\n", counter);
-                fclose(bus.file);
-                free(bus.content);
-                free_stack(*head);
-                exit(EXIT_FAILURE);
-        }
-
-        (*head)->next->n += (*head)->n;
-        pop(head, counter);
+	if (*head == NULL || (*head)->next == NULL)
+	{
+		fprintf(stderr, "L%u: can't add, stack too short\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	(*head)->next->n += (*head)->n;
+	pop(head, counter);
 }
 
 
