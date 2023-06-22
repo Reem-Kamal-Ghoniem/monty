aea2fc937c4191cb7c11f2d4f3d5a16292d08ca2
Haneenmohamed8 <125806698+Haneenmohamed8@users.noreply.github.com>
2023-06-23 01:58:46 +0300

    Update monty.h

--- monty.h
+++ monty.h
@@ -61,4 +61,5 @@ void pop(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
 void add(stack_t **head, unsigned int counter);
 void nop(stack_t **head, unsigned int counter);
+void free_stack(stack_t *head)
 #endif
