bab8aa6b4a50ce2d7f7ec59eb0e7911d881beea0
Haneenmohamed8 <125806698+Haneenmohamed8@users.noreply.github.com>
2023-06-23 04:50:55 +0300

    Create opcodes1.c

---
+++ opcodes1.c
@@ -0,0 +1,116 @@
+#include "monty.h"
+
+/**
+ * sub - subtraction
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+ */
+void sub(stack_t **head, unsigned int counter)
+{
+	stack_t *aux;
+	int sub, nodes;
+
+	aux = *head;
+	nodes = 0;
+	while (aux != NULL)
+	{
+		nodes++;
+		aux = aux->next;
+	}
+
+	if (nodes < 2)
+	{
+		fprintf(stderr, "L%u: can't sub, stack too short\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+
+	aux = *head;
+	sub = aux->next->n - aux->n;
+	aux->next->n = sub;
+	*head = aux->next;
+	free(aux);
+}
+
+
+
+
+/**
+ * div - divides the top two elements of the stack.
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+*/
+void div(stack_t **head, unsigned int counter)
+{
+	stack_t *h;
+	int len = 0, aux;
+
+	h = *head;
+	do
+	{
+		h = h->next;
+		len++;
+	} while (h != NULL);
+
+	if (len < 2)
+	{
+		fprintf(stderr, "L%d: can't div, stack too short\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+
+	h = *head;
+	if (h->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+
+	aux = h->next->n / h->n;
+	h->next->n = aux;
+	*head = h->next;
+	free(h);
+}
+
+
+
+/**
+ * mul - multiplies the top two elements of the stack.
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+*/
+void mul(stack_t **head, unsigned int counter)
+{
+	stack_t *h;
+	int len = 0, aux;
+
+	h = *head;
+	while (h)
+	{
+		h = h->next;
+		len++;
+	}
+	if (len < 2)
+	{
+		fprintf(stderr, "L%d: can't mul, stack too short\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	h = *head;
+	aux = h->next->n * h->n;
+	h->next->n = aux;
+	*head = h->next;
+	free(h);
+}
