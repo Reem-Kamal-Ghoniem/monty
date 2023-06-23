bb8cb2bbb19473ac7afb8f958c69fc67fd852cb6
Haneenmohamed8 <125806698+Haneenmohamed8@users.noreply.github.com>
2023-06-23 05:44:22 +0300

    Update monty.h

--- monty.h
+++ monty.h
@@ -76,4 +76,5 @@ void f_pstr(stack_t **head, unsigned int counter);
 void f_pchar(stack_t **head, unsigned int counter);
 void f_rotr(stack_t **head, __attribute__((unused)) unsigned int counter);
 void f_rotl(stack_t **head, __attribute__((unused)) unsigned int counter);
+void f_stack(stack_t **head, unsigned int counter);
 #endif
